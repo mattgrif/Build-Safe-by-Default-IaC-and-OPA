a05916014111f8180066c72a74a45b1ce2954197
Matt Griffin <MGriffin@misoenergy.org>
2026-08-06 15:24:16 -0400

    Test plan workflow

--- demo/terraform/example.tf
+++ demo/terraform/example.tf
@@ -4,15 +4,15 @@ resource "azurerm_resource_group" "rg-example" {
   location = "East US"
 }
 
-# Create a storage account with default configuration
-resource "azurerm_storage_account" "stexample" {
-  name                     = "stexample"
-  resource_group_name      = azurerm_resource_group.rg-example.name
-  location                 = azurerm_resource_group.rg-example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# # Create a storage account with default configuration
+# resource "azurerm_storage_account" "stexample" {
+#   name                     = "stexample"
+#   resource_group_name      = azurerm_resource_group.rg-example.name
+#   location                 = azurerm_resource_group.rg-example.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-  tags = {
-    environment = "Demo"
-  }
-}
+#   tags = {
+#     environment = "Demo"
+#   }
+# }
